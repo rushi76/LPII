1f3ecb16f07dbc30aa949933b0bce6a40d70a30e
rushi76 <141477090+rushi76@users.noreply.github.com>
2025-11-14 10:15:48 +0530

    Add files via upload

---
+++ Greedy_SelectionSort.py
@@ -0,0 +1,18 @@
+def selectionSort(array, size):
+    for step in range(size):
+        min_idx = step
+        for i in range(step + 1, size):
+            # select the minimum element in each loop
+            if array[i] < array[min_idx]:
+                min_idx = i
+    # put min at the correct position
+        (array[step], array[min_idx]) = (array[min_idx], array[step])
+    
+#main code
+data = [2, 45, 0, 11, 9]
+size = len(data)
+selectionSort(data, size)
+print('Sorted Array in Ascending Order:')
+
+for element in data:
+    print(element)
